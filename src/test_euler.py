61c80a234170d1527799eb9383a53a6907c872c0
alexandru-dinu <alex.dinu07@gmail.com>
2025-11-02 17:50:18 +0200

    (example) add euler/p6

--- src/test_euler.py
+++ src/test_euler.py
@@ -15,6 +15,6 @@ def euler_root():
 
 
 def test_euler(euler_root):
-    true = [233168, 4613732, 6857, 906609, 232792560]
+    true = [233168, 4613732, 6857, 906609, 232792560, 25164150]
     for i, res in enumerate(true, start=1):
         assert res == next(spool((euler_root / f"p{i}.spl").read_text()))
